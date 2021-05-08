5eb5034d085fc52a29251ea22d1782ad002caed9
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2021-05-08 19:12:52 +0300

    Pick and drop
    Add Ugly script for pick and drop -- need to fix

---
+++ BarrleTownn/Assets/Scripts/CollectItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItem : MonoBehaviour
+{
+    public VillagerCharacter player;
+
+    public Transform ItemLocation;
+    public Transform currentItem;
+
+    public bool isBarrel;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (isBarrel)
+        {
+            if (other.collider.CompareTag("Player") && player.GETIsPicked)
+            {
+                transform.parent = player.gameObject.transform;
+            }
+        }
+    }
+
+    public void PickedUp(GameObject item)
+    {
+
+
+    }
+
+}
--- BarrleTownn/Assets/Scripts/VillagerCharacter.cs
+++ BarrleTownn/Assets/Scripts/VillagerCharacter.cs
@@ -10,12 +10,27 @@ public class VillagerCharacter : MonoBehaviour
     float vertical;
     Rigidbody2D rb2D;
     private bool isFacingRight;
-    private Inventory inventory;
+    private bool isFacingUp;
+    public CollectItem item;
+    private bool isPicked;
+    public float distance;
 
-    private void Awake()
+    public GameObject box;
+
+    public bool GETIsPicked
     {
-        inventory = new Inventory();
-
+        set
+        {
+            if (isPicked != value)
+            {
+                isPicked = value;
+                
+            }
+        }
+        get
+        {
+            return isPicked;
+        }
     }
 
     private void Start()
@@ -26,38 +41,77 @@ public class VillagerCharacter : MonoBehaviour
     public virtual void Update()
     {
         MovementHandler();
+        PickUp();
+
+
     }
 
 
     public virtual void FixedUpdate()
     {
-        if (horiznotal!=0 && vertical!=0) //Diagnoal movement limited makes the movement more pleasent
+        if (horiznotal != 0 && vertical != 0) //Diagnoal movement limited makes the movement more pleasent
         {
             horiznotal *= 0.7f;
             vertical *= 0.7f;
         }
+
         rb2D.velocity = new Vector2(horiznotal * speed, vertical * speed);
 
-        Flip(horiznotal);
+        Flip(horiznotal,vertical);
 
     }
 
 
 
-    public void Flip(float horiznotal)
+    public void Flip(float horiznotal,float vertical)
+    {
+        if (horiznotal > 0 && !isFacingRight || horiznotal < 0 && isFacingRight)
         {
-            if (horiznotal > 0 && !isFacingRight || horiznotal < 0 && isFacingRight)
-            {
-                isFacingRight = !isFacingRight;
+            isFacingRight = !isFacingRight;
 
-                Vector3 scale = transform.localScale;
+            Vector3 scale = transform.localScale;
 
-                scale.x *= -1;
-                transform.localScale = scale;
-            }
+            scale.x *= -1;
+            transform.localScale = scale;
         }
 
-    
+        //if (vertical > 0 &&!isFacingUp || vertical<0 && isFacingUp)
+        //{
+        //    isFacingUp = !isFacingUp;
+
+        //    Vector3 scale = transform.localScale;
+        //    scale.y *= -1;
+        //    transform.localScale = scale;
+        //}
+    }
+
+    public virtual void PickUp()
+    {
+        Physics2D.queriesStartInColliders = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left * transform.localScale,distance);
+
+        if (Input.GetKeyDown(KeyCode.Q) && hit.collider != null)
+        {
+            GETIsPicked = true;
+            box = hit.collider.gameObject;
+           box.transform.parent = gameObject.transform;
+           speed = speed / 2;
+        }
+        else if (Input.GetKeyUp(KeyCode.Q)&& GETIsPicked)
+        {
+            box.transform.parent = null;
+            speed = speed * 2;
+            GETIsPicked = false;
+
+        }
+
+    }
+
+    private void OnDrawGizmos()  //to see the RayCast
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.left * transform.localScale * distance);
+    }
 
     public virtual void MovementHandler()
     {
