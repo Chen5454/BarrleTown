92142f1385db09744a4e97b9b43c0fc3f085af3f
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2021-05-23 18:27:57 +0300

    adding var
    Adding just bush+barrle

--- BarrleTownn/Assets/Scripts/InteractItem.cs
+++ BarrleTownn/Assets/Scripts/InteractItem.cs
@@ -8,32 +8,34 @@ public class InteractItem : MonoBehaviour
 {
     public VillagerCharacter player;
     public bool canHide;
-
+    public bool barrel;
+    public bool bush;
     public SpriteRenderer spriteRenderer;
 
     private void OnCollisionStay2D(Collision2D other)
     {
-
-        if (other.collider.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        if (barrel)
         {
-            canHide = true;
-            spriteRenderer.sortingOrder = 2;
-            player.playeRenderer.enabled = false;
-            //player.transform.position = gameObject.transform.position;
-            player.canMove = false;
+            if (other.collider.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+            {
+                canHide = true;
+                spriteRenderer.sortingOrder = 2;
+                player.playeRenderer.enabled = false;
+                //player.transform.position = gameObject.transform.position;
+                player.canMove = false;
+            }
+            else if (Input.GetKeyUp(KeyCode.E))
+            {
+                canHide = false;
+                spriteRenderer.sortingOrder = 0;
+                player.canMove = true;
+                player.playeRenderer.enabled = true;
+            }
         }
-        else if (Input.GetKeyUp(KeyCode.E))
-        {
-            canHide = false;
-            spriteRenderer.sortingOrder = 0;
-            player.canMove = true;
-            player.playeRenderer.enabled = true;
-
-
-        }
-
-
-
     }
+
+
+
+
     
 }
