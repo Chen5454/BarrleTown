644400ec904c465b4cb91bcd46905bc3d6113836
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2021-05-29 13:26:32 +0300

    switching wereWolf-villager
    switching wereWolf-villager

---
+++ BarrleTownn/Assets/WerewolfTransform.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class WerewolfTransform : MonoBehaviour
+{
+    public GameObject villager;
+    public GameObject wereWolf;
+    //public bool isWerewolf;
+    public int formSwitch;
+
+    private void Awake()
+    {
+        villager.SetActive(true);
+        wereWolf.SetActive(false);
+        formSwitch = 1;
+    }
+
+    //public bool getIsWereWolf
+    //{
+    //    set
+    //    {
+    //        if (isWerewolf != value)
+    //        {
+    //            isWerewolf = value;
+    //            villager.SetActive(false);
+    //            wereWolf.SetActive(true);
+
+    //        }
+    //        else 
+    //        {
+    //            villager.SetActive(true);
+    //            wereWolf.SetActive(false);
+    //        }
+    //    }
+
+    //    get
+    //    {
+    //        return isWerewolf;
+    //    }
+    //}
+
+
+    private void Update()
+    {
+        Transformation();
+    }
+
+    public void Transformation()
+    {
+        if (Input.GetKeyDown(KeyCode.T) && formSwitch == 1)
+        {
+
+            villager.SetActive(false);
+            wereWolf.SetActive(true);
+            formSwitch = 2;
+            wereWolf.transform.position = villager.transform.position;
+            wereWolf.transform.rotation = villager.transform.rotation;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.T) && formSwitch == 2)
+        {
+
+            villager.SetActive(true);
+            wereWolf.SetActive(false);
+            formSwitch = 1;
+            villager.transform.position = wereWolf.transform.position;
+            villager.transform.rotation = wereWolf.transform.rotation;
+
+
+        }
+    }
+}
